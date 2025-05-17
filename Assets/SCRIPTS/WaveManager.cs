4fdfb67442b913d1877d217a22c703f5d9487ac8
Harry Franklin <hrfranklin0702@gmail.com>
2025-05-17 03:29:33 +0100

    Early 3D Waves System
    Sine wave working and Gerstner wave shader developed but need to make Gerstner work with the physics and wave manager

---
+++ Assets/SCRIPTS/WaveManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    public static WaveManager instance;
+
+    public float amplitude = 1.0f;
+    public float length = 2.0f;
+    public float speed = 1.0f;
+    public float offset = 0f;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Instance already exists, destroying this one.");
+            Destroy(this);
+        }
+    }
+
+    private void Update()
+    {
+        offset += Time.deltaTime * speed;
+    }
+
+    public float GetWaveHeight(float x)
+    {
+        return amplitude * Mathf.Sin((x / length) + offset);
+    }
+}
